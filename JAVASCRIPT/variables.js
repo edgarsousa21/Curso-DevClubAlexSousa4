28fe31bb74946914323ee222c7a4385fec1fd223
Alex Sousa <alecs.sousa44@gmail.com>
2022-02-09 22:55:44 -0300

    commit for projects in javascript

---
+++ JAVASCRIPT/variables.js
@@ -0,0 +1,19 @@
+// Permitido:
+// - Iniciar com $ ou _
+// - Colocar acentos
+// - Iniciar com letras
+// - Letras maíusculas e minúsculas
+
+
+// Não pode:
+// - Iniciar com números
+// - Colocar espaços
+
+
+// Ideal:
+// - Camel case euEscrevoAssim
+// - Snake case eu_escrevo_assim
+// - SEMPRE em inglês
+// - Nomes que fazem sentido
+
+
