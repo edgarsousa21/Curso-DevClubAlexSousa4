22948acce07785423ea858498103863dc2f0408b
AlexSousa4 <alecs.sousa44@gmail.com>
2022-04-27 17:30:20 -0300

    Update JS

--- JAVASCRIPT/Exercises JS02/ex04.js
+++ JAVASCRIPT/Exercises JS02/ex04.js
@@ -1,7 +1,15 @@
-// [ ] Faça um programa que receba 3 números inteiros, e deve imprimir, qual é o maior, qual é o menor.
+// [X] Faça um programa que receba 3 números inteiros, e deve imprimir, qual é o maior, qual é o menor.
 
 
 const firstNumber = 3
 const seccondNumber = 5
 const thirdNumber = 7
+
+if (firstNumber) {
+    console.log("Esse é o menor número.")    
+}
+ 
+if (thirdNumber) {
+    console.log("Esse é o maior número.")
+}
 
---
+++ JAVASCRIPT/Exercises JS02/ex05.js
@@ -0,0 +1,35 @@
+// [ ] Crie 5 objetos nesse formato
+// { nome: "" idade:"" sexo:"" profissão: "" nacionalidade:"" }
+// Ao rodar o programa, deve imprimir se a pessoa é aprovada ou não no processo ,a pessoa deve ser maior de idade e brasileira para ser aprovada.
+
+const person = {
+    name: "Alex", 
+    age: 21,
+    sex: "Male",
+    profession: "counter clerk",
+    nationality: "Brazilian"
+}
+
+const person3 = {
+    name: "fabiane",
+    age: 24,
+    sex: "Female",
+    profession: "holistic therapist",
+    nationality: "Brazilian"
+}
+
+const person4 = {
+    name: "thiago",
+    age: 35,
+    sex: "Male",
+    profession: "Driver",
+    nationality: "Brazilian"
+}
+
+const person5 = {
+    name: "Lucia",
+    age: 41,
+    sex: "Female",
+    profession: "housewife",
+    nationality: "Brazilian"
+}
