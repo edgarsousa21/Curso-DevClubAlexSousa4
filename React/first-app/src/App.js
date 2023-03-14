d66b0bcc6ef49b05f434f4e7d37b5ae2082e407e
Alex Sousa <alex>
2023-03-14 11:26:56 -0300

    update

--- React/first-app/src/App.js
+++ React/first-app/src/App.js
@@ -1,29 +1,53 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import axios from 'axios'
 import People from './Assets/people.svg'
 import Arrow from './Assets/arrow.svg'
 import Trash from './Assets/trash.svg'
-import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from "./styles";
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button, User }
+  from "./styles";
 
 function App() {
-  const user = ({ id: Math.random(), name: "Alex", Age: 23 },
-    { id: Math.random(), name: "Alex", Age: 23 })
+  const [user, setUser] = useState([]);
+  const inputName = useRef();
+  const inputAge = useRef();
+  //primeira posição criar a variavel
+  //segunda posição função auxiliar a atualizar a variavel
+
+
+  async function addNewUser() {
+    // TODA VEZ QUE FOR USAR O AWAIT, O ASYNC TEM QUE ESTAR JUNTO.
+    const data = await axios.post("http://localhost:3001/users", {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+    })
+    console.log(data)
+  }
+
+  function deleteUser(userId) {
+    const newUser = user.filter(user => user.id !== userId)
+    setUser(newUser);
+  }
+
+
   return (
     <Container>
       <Image alt="logo-imagem" src={People} />
       <ContainerItens>
         <H1>Olá</H1>
         <InputLabel>Nome</InputLabel>
-        <Input placeholder="Nome"></Input>
+        <Input ref={inputName} placeholder="Nome"></Input>
 
         <InputLabel>Idade</InputLabel>
-        <Input placeholder="Idade"></Input>
+        <Input ref={inputAge} placeholder="Idade"></Input>
 
-        <Button>Cadastrar <img alt="seta" src={Arrow}></img></Button>
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow}></img></Button>
         <ul>
-          {user.map(user => (
+          {user.map((user) => (
             <User key={user.id}>
-              <p>{user.name} - {user.age}</p>
-              <button><img src={Trash} alt = "lata-de-lixo"/></button>
+              <p>{user.name} </p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt="lata-de-lixo" />
+              </button>
             </User>
           ))}
         </ul>
--- React/first-app/src/index.js
+++ React/first-app/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyle from './globalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<>
-        <App /> <GlobalStyle /> 
-            </>);
+    <App /> <GlobalStyle />
+</>);
+
+//todos compomentes react sempre precisam estar dentro de algo. Nada pode ficar
+//fora de um elemento pai!
